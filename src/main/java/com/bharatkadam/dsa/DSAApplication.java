8feefecc4d4fdfe28ddb70087fe1ce336a2a3194
IshanEnrique <rahul.kumar0850@gmail.com>
2024-04-16 00:31:29 +0530

    DSA

--- src/main/java/com/bharatkadam/dsa/DSAApplication.java
+++ src/main/java/com/bharatkadam/dsa/DSAApplication.java
@@ -1,27 +1,15 @@
 package com.bharatkadam.dsa;
-
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.stream.Stream;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 @SpringBootApplication
 public class DSAApplication implements CommandLineRunner {
 
-	@Value("${dataPath}")
-	private String path;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DSAApplication.class, args);
+		System.out.println("\n\n******************** DSA APPLICATION STARTED SUCCESSFULLY *****************************\n\n");
 	}
 
 	@Override
