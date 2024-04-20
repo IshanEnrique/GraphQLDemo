f88303206a9a0514f310ca68f3706f1765f13878
IshanEnrique <rahul.kumar0850@gmail.com>
2024-04-21 00:20:55 +0530

    REMOVED CREATE METHOD

--- src/main/java/com/bharatkadam/dsa/tree/BinaryTree.java
+++ src/main/java/com/bharatkadam/dsa/tree/BinaryTree.java
@@ -1,33 +1,14 @@
 package com.bharatkadam.dsa.tree;
 
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.util.CollectionUtils;
 
 public class BinaryTree extends Tree implements BaseTree{
    private static Logger LOGGER=LoggerFactory.getLogger(BinaryTree.class);
    public Tree.Node<?> root;
-   private Integer size;
-    public  <T> Node<?> createBinaryTree(List<T> data){
-        Integer position=size;
-        if(null==position){
-            position=-1;
-        }
-        position++;
-        if(CollectionUtils.isEmpty(data)|| position>=data.size()||null==data.get(position))
-        {
-            return null;
-        }
-        Tree.Node<?> node=createNode(data.get(position));
-        size=position;
-        node.left=createBinaryTree(data);
-        node.right=createBinaryTree(data);
-        return node;
-    }
     public static void traverse(BinaryTree binaryTree){
         if(null==binaryTree|| null==binaryTree.root){
             LOGGER.info(">>>>>>>>>>> \n\n\n NOTHING TO TRAVERSE IN THE GIVEN BINARY TREE. ");
